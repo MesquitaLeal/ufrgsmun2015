6df95f48bf1a14fb727d06bd4e062af47f0717da
mesquitaleal <mesquitaleal@gmail.com>
2015-10-07 07:39:00 -0300

    [NEW] Accommodation section

---
+++ conference/accommodation.php
@@ -0,0 +1,123 @@
+<div id="accommodation" class="background">
+    <div class="row gray-70 content">
+        <div class="breadcrumb">
+            <p>home > conference > accommodation</p>
+        </div>
+        <div class="section-title">
+            <h2>Accommodation</h2>
+        </div>
+    </div>
+<!--<svg class="gray-70-svg" xmlns="http://www.w3.org/2000/svg" version="1.1" width="100%" height="40" viewBox="0 0 100 102" preserveAspectRatio="none">
+    <path d="M0 0 L50 100 L100 0 Z"></path>
+</svg>-->
+    <div class="row gray-50 section-p content">
+        <div class="col-md-12">
+            <p class="center content-medium">
+                The official accommodation sites of this year’s edition are the Master Express Alberto Bins Hotel, less than a 10 Min. walk from the Conference venue, and will grant special fares for UFRGSMUN participants. Additionally, delegates that decide to stay there will have free transportation to and from social events. Besides Master, there are other options nearby as well. The prices shown below are per person, per night. If you wish to get these prices, you must tell the place when you book that it’s for UFRGSMUN.
+                <br/>Furthermore, we also offer the opportunity of staying in the house of one of the staff members. This free accommodation is very limited and cannot be guaranteed to all who are interested, but we will do our best. The form will be available mid-September.
+            </p>
+        </div>
+    </div>
+</div>
+
+<div class="row gray content mainAccommodation-row">
+    <div class="col-md-4">
+        <img src="images/hotel.png" alt="Master Hotel" class="img-responsive" />
+        <h3>The Master Express Alberto Bins</h3>
+        <p>
+            Single: R$ 106.00<br/>
+            Double: R$ 136.00<br/>
+            SPECIAL 5-DAY DOUBLE (with breakfast): R$ 340.00
+        </p>
+        <p class="mainAccommodation-address">
+            Av. Alberto Bins, 618<br/>
+            Centro Histórico, Porto Alegre - RS<br/>
+            (51) 3025-4000
+        </p>
+    </div>
+    <div class="col-md-8">
+        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3454.3129328057394!2d-51.2204343!3d-30.027878899999987!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x951979a7e541efb3%3A0x77b151594be83b65!2sHotel+Master+Express+Alberto+Bins!5e0!3m2!1spt-BR!2sbr!4v1444183136258" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
+    </div>
+</div>
+
+<div class="row gray content secondaryAccommodation-row">
+    <div class="col-md-4">
+        <h3>Ibis Budget</h3>
+        <p class="secondaryAccommodation-rates">
+            Double: R$149.00 (breakfast R$14.00)
+        </p>
+        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d55264.78526316241!2d-51.22653775!3d-30.03544979999999!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x95197909f09688b9%3A0x11bc4ae7b1b68ede!2sIbis+budget!5e0!3m2!1spt-BR!2sbr!4v1444184329773" height="300" frameborder="0" style="border:0" allowfullscreen></iframe>
+        <p class="secondaryAccommodation-address">
+            Av. Júlio de Castilhos, 516<br/>
+            Centro Histórico, Porto Alegre - RS<br/>
+            (51) 3515.6444
+        </p>
+    </div>
+    <div class="col-md-4">
+        <h3>Master Express Sarmento Leite</h3>
+        <p class="secondaryAccommodation-rates">
+            Double: R$180.00<br/>
+            Triple: R$210.00
+        </p>
+        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3454.012107616134!2d-51.22345310000006!3d-30.036510499999995!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x95197900f8f68709%3A0x8ac15ee326bfeead!2sHotel+Master+Express+Cidade+Baixa!5e0!3m2!1spt-BR!2sbr!4v1444185427179" height="300" frameborder="0" style="border:0" allowfullscreen></iframe>
+        <p class="secondaryAccommodation-address">
+            Rua Sarmento Leite, 865<br/>
+            Cidade Baixa, Porto Alegre - RS<br/>
+            (51) 3018.3636
+        </p>
+    </div>
+    <div class="col-md-4">
+        <h3>Hotel Plaza São Rafael</h3>
+        <p class="secondaryAccommodation-rates">
+            Double: R$210.00<br/>
+            Triple: R$321.00
+        </p>
+        <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d3454.3128805351294!2d-51.2215513!3d-30.0278804!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x951979081d9ee991%3A0xc5dd42e6069272d3!2sPlaza+S%C3%A3o+Rafael!5e0!3m2!1spt-BR!2sbr!4v1444186560342" height="300" frameborder="0" style="border:0" allowfullscreen></iframe>
+        <p class="secondaryAccommodation-address">
+            Avenida Alberto Bins, 514<br/>
+            Centro Historico, Porto Alegre - RS<br/>
+            (51) 3220.7000
+        </p>
+    </div>
+</div>
+
+<div class="row gray content secondaryAccommodation-row">
+    <div class="col-md-4">
+        <h3>Eko Residence Hotel</h3>
+        <p class="secondaryAccommodation-rates">
+            Double: R$ 149.00<br/>
+            Triple: R$ 230.00
+        </p>
+        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d55264.78526316241!2d-51.22653775!3d-30.03544979999999!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x95197909f09688b9%3A0x11bc4ae7b1b68ede!2sIbis+budget!5e0!3m2!1spt-BR!2sbr!4v1444184329773" height="300" frameborder="0" style="border:0" allowfullscreen></iframe>
+        <p class="secondaryAccommodation-address">
+            Rua Desembargador André da Rocha, 131<br/>
+            Centro, Porto Alegre - RS<br/>
+            (51) 3215.7600
+        </p>
+    </div>
+    <div class="col-md-4">
+        <h3>Rock'n'Hostel</h3>
+        <p class="secondaryAccommodation-rates">
+            Bed Mixed Dorm: R$ 50.00<br/>
+            5 Bed Mixed Dorm: R$40.00
+        </p>
+        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3454.318152885416!2d-51.21694400000005!3d-30.027729100000002!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x951979a8a4eec209%3A0xe78131d371ccf841!2sRock&#39;n&#39;Hostel!5e0!3m2!1spt-BR!2sbr!4v1444187040468" height="300" frameborder="0" style="border:0" allowfullscreen></iframe>
+        <p class="secondaryAccommodation-address">
+            Av. Alberto Bins, 954<br/>
+            Centro Histórico, Porto Alegre - RS<br/>
+            (51) 3557.3523
+        </p>
+    </div>
+    <div class="col-md-4">
+        <h3>Hostel Porto do Sol</h3>
+        <p class="secondaryAccommodation-rates">
+            Double: R$149.00 (breakfast R$14.00)
+        </p>
+        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3454.0182945670367!2d-51.20384409999995!3d-30.036332999999992!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x9519784ba3080efd%3A0xa8c6f9f1c27b8f9!2sHostel+Porto+do+Sol!5e0!3m2!1spt-BR!2sbr!4v1444187129074" height="300" frameborder="0" style="border:0" allowfullscreen></iframe>
+        <p class="secondaryAccommodation-address">
+            R. Mariante, 958<br/>
+            Rio Branco, Porto Alegre - RS<br/>
+            (51) 3330.1324
+        </p>
+    </div>
+</div>
